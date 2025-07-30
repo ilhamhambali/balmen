3f36825a248a5e962295baaa6120fb5a7c9ca938
ilhamhambali <ilhamhambali2000@gmail.com>
2025-07-30 22:55:11 +0700

    konfigurasi vercel

--- api/index.php
+++ api/index.php
@@ -1,17 +1,3 @@
 <?php
-
-// api/index.php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
